bc54aa23b2ad3605e66672436f1643206611ccae
luka7711 <ulish6737@gmail.com>
2022-11-11 11:25:24 -0600

    Displaying search bar and icon in a row

--- src/components/Shared/SearchBar.js
+++ src/components/Shared/SearchBar.js
@@ -1,19 +1,48 @@
 import Icon from "./Icons"
 import { faMagnifyingGlassLocation } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+
+
+
+const formContainer = {
+    width: "100%",
+    height: "inherit",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: "0 15px",
+    background: "#fff",
+    borderRadius: "5px",
+    fontSize: "1.3rem"
+}
+
+const iconContainer = {
+    width: "10%",
+    height: "inherit",
+    display: "flex",
+    alignItems: 'center',
+    justifyContent: "flex-end",
+    color: '#bbb9b9'
+}
+
+const inputBox = {
+    width: '90%',
+    height: 'inherit',
+    border: "none"
+}
+
 
 
 
 export default function SearchBar() {
     return (
-        <div>
-            <div>
-                <form>
-                    <input type="text" placeholder="city" value="" onChange={() => ""}/>
+            <React.Fragment>
+                <form style={formContainer} >
+                    <input style={inputBox} autofocus="autofocus" type="text" placeholder="city" value="" onChange={() => ""}/>
+                    <span style={iconContainer}>
+                        <Icon icon={faMagnifyingGlassLocation} />
+                    </span>
                 </form>
-            </div>
-            <div>
-                <Icon icon={faMagnifyingGlassLocation} />
-            </div>
-        </div>
+            </React.Fragment>
     )
 }
