c992106f8e5f2909a2442ef1917b2b2037a52fed
luka7711 <ulish6737@gmail.com>
2022-11-10 19:14:34 -0600

    Rendered image on Home page

--- src/components/Shared/HomeSearchbarHolder.js
+++ src/components/Shared/HomeSearchbarHolder.js
@@ -1,9 +1,18 @@
 import SearchBar from "./SearchBar";
+import ImgMain from '../../images/realestate.jpg'
 
+const main = {
+    width: "100%",
+    height: "350px",
+    backgroundImage: `url(${ImgMain})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center'
+}
 
 export default function HomeSearchbarHolder() {
     return (
-        <main>
+        <main style={main}>
             <SearchBar/>
         </main>
     )
