b79f404c1219ec9bc9c4c58b324ef9ebb320c522
luka7711 <ulish6737@gmail.com>
2022-12-14 18:47:52 -0600

    Test deployment

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,7 +10,7 @@ const Navigation = () => {
                 <li className={styles.nav_li}><Link to={'/housing-list'}>Buy</Link></li>
             </ul>
             <ul>
-                <li className={styles.logo}><Link to={'/'}><span><FontAwesomeIcon icon={faBuilding}/></span>  Real Estate </Link></li>
+                <li className={styles.logo}><Link to={'/'}><span><FontAwesomeIcon icon={faBuilding}/></span>  real estate </Link></li>
             </ul>
       </nav>
     )
