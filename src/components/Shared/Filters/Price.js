0698ecc303bb615b44b892a4674dbf664d96274f
luka7711 <ulish6737@gmail.com>
2022-12-28 21:37:49 -0600

    Add input form for min and max price filter

--- src/components/Shared/Filters/Price.js
+++ src/components/Shared/Filters/Price.js
@@ -1,21 +1,46 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import uuid from "react-uuid";
 import { nFormatter } from "../../../utils";
 import { integerToThousands } from "../../../utils";
 
 
-function PriceRange({priceNumbers, formatter}) {
+function PriceRange({
+     priceNumbers, 
+     formatter, 
+     priceType,
+     initialVal
+    }) {
+    
+    const [price, setPrice] = useState(initialVal);
+
+    useEffect(() => {
+
+    }, [])
+
+
+
     const priceList = priceNumbers.map(price => {
-        return <li key={uuid()}>{formatter(price, 2)}</li>
+
+        const formattedPrice = formatter(price, 2)
+
+        return (
+            <li onClick={() => setPrice(price)} key={uuid()}>
+                { formattedPrice }
+            </li>
+        )
     });
 
+
     return (
-        <React.Fragment>
-            <input placeholder=""/>
-            <ul key={uuid()}>
-                {priceList}
-            </ul>
-        </React.Fragment>
+        <>
+            <label>{priceType}</label>
+            <input key={uuid()} type="text" placeholder={price} value={price}/>
+            <section>
+                <ul>
+                    {priceList}
+                </ul>
+            </section>
+        </>
     )
 }
 
@@ -46,8 +71,18 @@ function PriceForm() {
         <div>
             <h5>Price Range</h5>
             <form>
-                <PriceRange priceNumbers={minPriceRange} formatter={integerToThousands}/>
-                <PriceRange priceNumbers={maxPriceRange} formatter={nFormatter}/>
+                <PriceRange 
+                    priceNumbers={minPriceRange} 
+                    formatter={integerToThousands} 
+                    priceType="Minimum"
+                    initialVal="No Min"
+                />
+                <PriceRange 
+                    priceNumbers={maxPriceRange} 
+                    formatter={nFormatter} 
+                    priceType="Maximum"
+                    initialVal="No Max"
+                />
             </form>
         </div>
     )
